19b9e914c83af3a5518b81e1dc68592061739512
Sofian Brabez <sbz@6dev.net>
2023-04-17 23:10:00 +0200

    Update vagrant box, hostname and extract fact with jq

--- getfacts.sh
+++ getfacts.sh
@@ -3,5 +3,16 @@
 [ -f ./facts ] && rm ./facts
 hostname=`hostname`
 
+cat <<'EOF' > reduce.jq
+def pick($paths): 
+  . as $root | reduce $paths[] as $path ({}; . + { ($path): $root[$path] }); 
+
+ .ansible_facts | pick(["ansible_kernel", "ansible_architecture", "ansible_distribution", "ansible_kernel_version"])
+EOF
+
+ansible -m setup localhost | sed '1 s/^.*|.*=>.*$/{/g' | jq -f reduce.jq
+
 echo -e "## auto generated with: \$ ansible -k -b -m setup ${hostname}" | tee ./facts
 ansible -k -b -m setup ${hostname} | tee -a ./facts
+
+rm -rf reduce.jq
